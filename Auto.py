7099f868aa568a01e0602ad11d532e6693de4eea
MHDZubalR <136376129+MHDZubalR@users.noreply.github.com>
2023-11-15 00:17:12 +0400

    Create Auto.py
    AutoUpdate BioScope Playlist

---
+++ Auto.py
@@ -0,0 +1,9 @@
+python
+import requests
+import time
+
+while True:
+    response = requests.get("https://mhdzubair.top/ARAFAT/biom3u8grab.php")
+    with open("output.txt", "w") as file:
+        file.write(response.text)
+    time.sleep(1800)  # 30 minutes interval
